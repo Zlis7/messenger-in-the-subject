36577557b51f24fbb4217eaeea956a68c8ca7716
Калмыков Денис Владимирович <23201071@live.preco.ru>
2025-02-07 11:32:19 +0500

    A1

--- authorization/models.py
+++ authorization/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Tokens(models.Model):
+    value = models.CharField(max_length=7)
+    is_activate = models.BooleanField()
--- authorization/urls.py
+++ authorization/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from authorization import views
 
 urlpatterns = [
-    path('', views.login, name='home'),
-    path('<slug:id>', views.authorization, name='chat')
+    path('login', views.login, name='home'),
+    path('regis', views.authorization, name='chat'),
+    path('register_confirm/<token>/', views.register_confirm, name='register_confirm')    
 ]

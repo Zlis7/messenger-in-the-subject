f8847f96766a519f9fdac0227329b5dd0ed70264
Калмыков Денис Владимирович <23201071@live.preco.ru>
2025-02-07 09:10:04 +0500

    a1

--- messenger/views.py
+++ messenger/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'messenger/html/index.html')
 
 def chat(request, id):
-    return render(request, 'messenger/html/chat.html')
+    return render(request, 'messenger/html/chat.html', {'name': 'gsdg'})

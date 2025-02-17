db292bde0e5603f840b56ba72b83b04d705e193c
Zlis7 <denkalmy@gmail.com>
2025-02-17 13:21:13 +0300

    release

--- api/settings.py
+++ api/settings.py
@@ -87,6 +87,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'authentication/static'),
+    os.path.join(BASE_DIR, 'messenger/static')
+]
 
 AUTH_USER_MODEL = 'authentication.MinUser'
 
